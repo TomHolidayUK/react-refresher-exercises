1cd630f82980114daddc3781dec1653f192f0576
Tom <tom.holiday@mac.com>
2025-04-14 23:23:51 +0100

    exercise 1 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter(counter + 1)
+  }
+
+  function resetCounter() {
+    setCounter(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Value: {counter}</p>
+      <button onClick={incrementCounter}>Increase</button>
+      <button onClick={resetCounter}>Reset</button>
     </div>
-  );
+  )
 }
 
 export default App;
