791f1826c34dd66bb8b380fcba4c212823b1d239
Tom Holiday <tom.holiday@mac.com>
2025-04-20 21:38:55 +0100

    end of exercise 3

--- src/Components/Exercise3.tsx
+++ src/Components/Exercise3.tsx
@@ -1,24 +1,17 @@
 import React, { useState } from "react";
+import { ChromePicker } from "react-color";
 
 export default function Exercise3() {
-  const colours = ["Red", "Green", "Blue"];
-
   const [colour, setColour] = useState("");
-
-  function changeBackground(colour: string) {
-    console.log(colour);
-    setColour(colour);
-  }
 
   return (
     <div style={{ backgroundColor: colour, height: "100vh" }}>
       <h2>Exercise 3: Choose background colour</h2>
-      {colours.map((colour, index) => (
-        <div key={index} onClick={() => changeBackground(colour)}>
-          {colour}
-        </div>
-      ))}
       <button onClick={() => setColour("")}>Reset background colour</button>
+      <ChromePicker
+        color={colour}
+        onChange={(updated) => setColour(updated.hex)}
+      />
     </div>
   );
 }
